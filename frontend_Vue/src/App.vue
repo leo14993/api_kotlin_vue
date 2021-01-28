76512dde5c9d668ddd233dfa3488bebd3f73c312
Leonardo Rodrigues <leonardolp_rj@hotmail.com>
2021-01-28 15:43:00 -0300

    chore(painel) criando um componente para imagem responsiva

--- frontend_Vue/src/App.vue
+++ frontend_Vue/src/App.vue
@@ -2,16 +2,18 @@
 
   <div class="corpo">
     <h1 class="centralizado"> {{ titulo }} </h1>
-    <label>
-      <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtro por parte do titulo">
 
-    </label>
+
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtro por parte do titulo">
     {{ filtro }}
+
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+
         <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
         </meu-painel>
+
       </li>
     </ul>
 
@@ -20,10 +22,12 @@
 
 <script>
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
   components: {
-    'meu-painel' : Painel
+    'meu-painel' : Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
 
   name: 'app',
@@ -77,10 +81,6 @@ export default {
 
   .lista-fotos .lista-fotos-item {
     display: inline-block;
-  }
-
-  .imagem-responsiva{
-    width: 100%;
   }
 
 </style>
