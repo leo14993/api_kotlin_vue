95e6a6e2fcfbec2cd16c6e274269534704affc6f
Leonardo Rodrigues <leonardolp_rj@hotmail.com>
2021-01-31 18:42:37 -0300

    refact(directives) passando mais de um parametro na diretiva atraves de um objeto

--- frontend_Vue/src/directives/Transform.js
+++ frontend_Vue/src/directives/Transform.js
@@ -7,8 +7,20 @@ Vue.directive('meu-transform',{
     let current = 0;
 
     el.addEventListener('click',function() {
-      current +=90;
+      let incremento = 90;
+      let animate = false;
+
+      if(binding.value) {
+        incremento = binding.value.incremento;
+        animate = binding.value.animate;
+      }
+
+      current += incremento;
       el.style.transform = `rotate(${current}deg)`;
+
+      if(animate){
+        el.style.transition = 'transform 0.5s'
+      }
     })
   }
 });
