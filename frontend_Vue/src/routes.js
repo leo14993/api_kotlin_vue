780e81b92133c41fbe06adb10b2aae37892bd311
Leonardo Rodrigues <leonardolp_rj@hotmail.com>
2021-02-02 17:00:33 -0300

    refact(routes) criando rota para novo usuario

--- frontend_Vue/src/routes.js
+++ frontend_Vue/src/routes.js
@@ -1,10 +1,12 @@
 import Cadastro from './components/cadastro/Cadastro.vue';
 import Home from './components/home/Home.vue';
+import NovoUsuario from './components/novo_usuario/NovoUsuario.vue';
 
 export const routes = [
   {path: '', name: 'home',component: Home, titulo: 'Home', menu: true },
   {path: '/cadastro', name: 'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true  },
   {path: '/cadastro/:id', name: 'altera', component: Cadastro, titulo: 'Cadastro', menu: false  },
+  {path: '/novo-usuario', name: 'altera', component: NovoUsuario, titulo: 'Novo Usuario', menu: true  },
   {path: '*', component: Home, menu: false },
 
 ];
