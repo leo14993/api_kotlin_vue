5d8340ff35f32bc44ea963997bebc5449e9c9d7e
Leonardo Rodrigues <leonardolp_rj@hotmail.com>
2021-02-02 16:56:41 -0300

    feat(NovoUsuario) criando componente de novo usuario

---
+++ frontend_Vue/src/components/novo_usuario/NovoUsuario.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+
+    <div>
+      <h1>Novo usuário.</h1>
+    </div>
+
+    <form @submit.prevent="enviarFormulario()">
+
+      <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" v-model="usuario.nome">
+      </div>
+
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="text" class="form-control" v-model="usuario.email">
+      </div>
+
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha">
+      </div>
+
+      <button class="btn btn-primary" type="submit">Salvar</button>
+
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  data: function() {
+    return {
+      usuario: {
+        nome: '',
+        senha: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    enviarFormulario ()  {
+        console.log(this.usuario);
+    }
+  }
+
+}
+</script>
