5953a44b379f3d7cb3bc42d5e797898b0f1d4ba1
Leonardo Rodrigues <leonardolp_rj@hotmail.com>
2021-02-03 20:24:42 -0300

    feat(gerentes) criando um componente de gerentes carregados por outra api
    - gerentes só são exibidos quando é realizado o login

--- frontend_Vue/src/routes.js
+++ frontend_Vue/src/routes.js
@@ -2,6 +2,7 @@ import Cadastro from './components/cadastro/Cadastro.vue';
 import Home from './components/home/Home.vue';
 import NovoUsuario from './components/novo_usuario/NovoUsuario.vue';
 import Login from './components/login/Login.vue';
+import Gerentes from './views/Gerentes.vue';
 
 export const routes = [
   {path: '', name: 'home',component: Home, titulo: 'Home', menu: true },
@@ -9,6 +10,7 @@ export const routes = [
   {path: '/cadastro/:id', name: 'altera', component: Cadastro, titulo: 'Cadastro', menu: false  },
   {path: '/novo_usuario', name: 'novo.usuario', component: NovoUsuario, titulo: 'Novo Usuario', menu: true  },
   {path: '/login', name: 'login', component: Login, titulo: 'Login', menu: true  },
+  {path: '/gerentes', name: 'gerentes', component: Gerentes, titulo: 'Gerentes', menu: true  },
   {path: '*', component: Home, menu: false },
 
 ];
---
+++ frontend_Vue/src/views/Gerentes.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1>Gerentes</h1>
+    <div class="row">
+      <Gerente v-for="gerente in gerentes" :key="gerente.numero" :gerente="gerente" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Gerente from '../components/gerentes/Gerente.vue'
+import users from '../connection/users'
+
+export default {
+  components: {
+    Gerente
+  },
+  data() {
+    return {
+      gerentes: []
+    }
+  },
+  mounted () {
+    users.get('gerentes')
+        .then(response => this.gerentes = response.data)
+        .catch(erro => console.log(erro))
+  }
+}
+</script>
+
+<style>
+
+</style>
